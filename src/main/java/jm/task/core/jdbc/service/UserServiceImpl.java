23eeb17e9e9ec23997604bc9ec829139269f114d
Айрат Латыпов <ayrat.latypov.2020@bk.ru>
2024-04-15 00:59:49 +0300

    fix

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,11 +1,11 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDaoHibernateImpl usrDao = new UserDaoHibernateImpl();
+    private final UserDaoJDBCImpl usrDao = new UserDaoJDBCImpl();
     public void createUsersTable() {
         usrDao.createUsersTable();
     }
